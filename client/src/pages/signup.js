14b14fc70d30ae8b28c72ddd669bdd16f5194cd3
Andrew Knowles <jaknowles1997@gmail.com>
2021-02-12 18:26:57 -0500

    Update and rename Signup.js to signup.js

--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -4,7 +4,7 @@ import { useMutation } from '@apollo/react-hooks';
 import Auth from "../utils/auth";
 import { ADD_USER } from "../utils/mutations";
 
-function Signup(props) {
+function signup(props) {
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [addUser] = useMutation(ADD_USER);
 
@@ -87,4 +87,4 @@ function Signup(props) {
 
 }
 
-export default Signup;
+export default signup;
