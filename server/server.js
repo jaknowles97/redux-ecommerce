9ad981a0a3453eab718e58c88917dd2e8a77e3ed
jaknowles97 <jaknowles1997@gmail.com>
2021-02-12 17:05:23 -0500

    set up client side boilerplate

--- server/server.js
+++ server/server.js
@@ -4,8 +4,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schemas');
-// TODO: create auth logic
-// const { authMiddleware } = require('./utils/auth');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
@@ -13,7 +12,7 @@ const app = express();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-//   context: authMiddleware
+  context: authMiddleware
 });
 
 server.applyMiddleware({ app });
@@ -22,7 +21,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 // Serve up static assets
-// app.use('/images', express.static(path.join(__dirname, '../client/images')));
+app.use('/images', express.static(path.join(__dirname, '../client/images')));
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
