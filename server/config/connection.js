5b50c8761160c3d803600c56a9d0c3a3b07ab725
jaknowles97 <jaknowles1997@gmail.com>
2021-03-04 13:01:54 -0500

    seeding atlas cluster

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
   
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/reduxstore', {
+mongoose.connect('mongodb+srv://admin:8279000Ak@cluster0.psc0s.mongodb.net/myFirstDatabase?retryWrites=true&w=majority' , {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
