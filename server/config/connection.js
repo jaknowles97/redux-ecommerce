1a2125709e477bee091f90fd0721fa1e8aa44189
jaknowles97 <jaknowles1997@gmail.com>
2021-03-04 14:15:30 -0500

    test

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+
 const mongoose = require('mongoose');
 
 mongoose.connect('mongodb+srv://admin:8279000Ak@cluster0.psc0s.mongodb.net/redux-ecommerce?retryWrites=true&w=majority', {
